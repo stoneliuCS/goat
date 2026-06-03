c93792e50ef1477b9c120bbedbec390b83f11855
Stone Liu <liu.sto@northeastern.edu>
2026-06-02 22:46:06 -0400

    XOR

--- perceptron.go
+++ perceptron.go
@@ -35,14 +35,38 @@ func (this *Perceptron) Forward(inputs ...Input) uint8 {
 }
 
 func NAND(x1 uint8, x2 uint8) uint8 {
-	if x1 != 0 || x1 != 1 {
+	if x1 != 0 && x1 != 1 {
 		panic("X1 must be a bit")
 	}
 
-	if x2 != 0 || x2 != 1 {
+	if x2 != 0 && x2 != 1 {
 		panic("X2 must be a bit")
 	}
 
 	p1 := CreatePerceptron(3, 2)
 	return p1.Forward(Input{X: x1, W: -2}, Input{X: x2, W: -2})
+}
+
+func XOR(inputs ...uint8) uint8 {
+	if len(inputs) != 2 {
+		panic("Input must be of size 2")
+	}
+	x1 := Input{X: inputs[0], W: -2}
+	x2 := Input{X: inputs[1], W: -2}
+	var bias float64 = 3
+	p1 := CreatePerceptron(bias, 2)
+	p2 := CreatePerceptron(bias, 2)
+	p3 := CreatePerceptron(bias, 2)
+	p4 := CreatePerceptron(bias, 2)
+
+	o1 := p1.Forward(x1, x2)
+	input_1 := Input{X: o1, W: -2}
+
+	o2 := p2.Forward(x1, input_1)
+	input_2 := Input{X: o2, W: -2}
+
+	o3 := p3.Forward(x2, input_1)
+	input_3 := Input{X: o3, W: -2}
+
+	return p4.Forward(input_2, input_3)
 }
--- perceptron_test.go
+++ perceptron_test.go
@@ -24,4 +24,15 @@ func TestNANDGate(t *testing.T) {
 }
 
 func TestXOR(t *testing.T) {
+	// 0 XOR 0 => 0
+	assert.Equal(t, uint8(0), nn.XOR(0, 0))
+
+	// 1 XOR 0 => 1
+	assert.Equal(t, uint8(1), nn.XOR(1, 0))
+
+	// 0 XOR 1 => 1
+	assert.Equal(t, uint8(1), nn.XOR(0, 1))
+
+	// 1 XOR 1 => 0
+	assert.Equal(t, uint8(0), nn.XOR(1, 1))
 }
